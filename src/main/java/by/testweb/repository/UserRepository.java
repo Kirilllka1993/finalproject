c3a99e45e45fab8295d42219d7463fff1d6568a1
kirilllka1993 <kirilllka1993@gmail.com>
2024-07-18 20:26:22 +0300

    Hello

--- src/main/java/by/testweb/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package by.testweb.repository;
-
-import by.testweb.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findByUserName(String userName);
-}
